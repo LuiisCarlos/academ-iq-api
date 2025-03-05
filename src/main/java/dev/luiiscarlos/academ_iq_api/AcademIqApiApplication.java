6b0988f0def06b75687c58e92d670cc6ed6b2661
LuiisCarlos <luisccg2003@gmail.com>
2025-03-05 23:04:03 +0100

    clean structure and properties changes

---
+++ src/main/java/dev/luiiscarlos/academ_iq_api/AcademIqApiApplication.java
@@ -0,0 +1,13 @@
+package dev.luiiscarlos.academ_iq_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademIqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademIqApiApplication.class, args);
+	}
+
+}
